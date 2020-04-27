431b3d18f8d04707ac58d5e5b89222dbb717735a
Arsalan RPI <arsalan.sahab@gmail.com>
2020-04-27 16:20:35 +0500

    added Problem Definition

---
+++ eq_max.py
@@ -0,0 +1,23 @@
+'''
+Problem : Maxmise Output of given equation
+
+Given Equation : 2x + 2y
+
+Techniques : Evolutionary Algortihm > Genetic Algorithm
+Sub-Techniques Required : 
+
+    1. Parent Selection > Binary Tournament
+    2. Crossover 
+    3. Mutation
+    4. Survivor Selection > Truncation
+
+Extra Info : 
+
+    1. Produce 4 Off Springs
+    2. CrossOver = x(Parent 1), y(Parent 2)
+    3. Mutation Rate = 50%
+    4. 20 Generations
+
+
+
+'''
