71f81ea19ec317fae7a88755aed03be49cb42fa2
Arsalan RPI <arsalan.sahab@gmail.com>
2020-05-01 04:04:39 +0500

    Ran first succesful test

--- eq_max.py
+++ eq_max.py
@@ -1,3 +1,12 @@
+####### IMPORTS ########
+
+import random
+import genetic_algorithm as ga
+
+
+
+
+
 '''
 Problem : Maxmise Output of given equation
 
@@ -31,14 +40,93 @@ Chromosome 5 = {-3,3}
 
 '''
 
-print("this shit is workin bro")
 
- '''
-    parents = numpy.empty((num_parents, population.shape[1]))
-    for parent_num in range(num_parents):
-        max_fitness_idx = numpy.where(fitness_val == numpy.max(fitness_val))
-        max_fitness_idx = max_fitness_idx[0][0]
-        parents[parent_num, :] = population[max_fitness_idx, :]
-        fitness_val[max_fitness_idx] = -99999999999
 
-    return parents '''
+
+def function_init(chromosome):
+
+    return ((chromosome[0]*chromosome[0]) + (chromosome[1]*chromosome[1]))
+
+
+
+def ga_driver(population):
+
+
+    new_generation = []
+
+    for i in range(20):
+
+        for j in range(3):
+
+            # Get 4 Random numbers
+
+            index_1 = random.randint(0,3)
+            index_2 = random.randint(0,3)
+            index_3 = random.randint(0,3)
+            index_4 = random.randint(0,3)
+
+            
+            #print("Index 1 = " + str(index_1))
+           
+
+
+            # Perform Binary Tournament
+
+            parent_1 = ga.binary_tournament(population[index_1],population[index_3])
+            parent_2 = ga.binary_tournament(population[index_4],population[index_2])
+
+
+            # Generate OffSprings 
+
+            offspring_1 = ga.crossover(parent_1,parent_2)
+            offspring_2 = ga.crossover(parent_2,parent_1)
+
+
+            new_generation.append(offspring_1)
+            new_generation.append(offspring_2)
+
+        population = new_generation
+        
+        ga.truncate(population)
+
+        print("Current Generation Statistics : " )
+        print("Generation = " + str((i+1)))
+        print("Poluation Lenght : " + str(len(population)))
+        print("Elements : " ,population)
+        print("Highest Fitness Value = " + str(ga.fitness(ga.function,population[0])))
+
+
+
+
+
+
+
+def main():
+
+
+    ###### INITIALISATIONS #######
+
+    population = [[1,2],[-2,3],[4,-1],[5,2],[-3,3]]
+   
+    #function = lambda x,y : (x)**2 + (y)**2
+
+    population.sort(key=function_init,reverse=True)
+
+    print("Current Generation Statistics : " )
+    print("Generation = 0")
+    print("Elements : " ,population)
+    print("Highest Fitness Value = " + str(ga.fitness(ga.function,population[0])))
+
+
+    ga_driver(population)
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    main()
--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -2,18 +2,13 @@
 
 import numpy
 import random
-
-
-###### INITIALISATIONS #######
-
-population = [[1,2],[-2,3],[4,-1],[5,2],[-3,3]]
-#population.astype(int)
-function = lambda x,y : (x)*2 + (y)*2
-
+import eq_max
 
 ####### Fitness Function #########
 
-def fitness(chromosome):
+function = lambda x,y : (x)**2 + (y)**2
+
+def fitness(function,chromosome):
 
     fitness_val = function(chromosome[0],chromosome[1])
 
@@ -24,56 +19,52 @@ def fitness(chromosome):
 def binary_tournament(parent_a,parent_b):
 
 
-    if(fitness(parent_a) > fitness(parent_b)):
+    if(fitness(function,parent_a) > fitness(function,parent_b)):
 
         return parent_a
     else :
 
         return parent_b
 
+    return None
+
 ######## Crossover #############
 def crossover(parent_a,parent_b):
 
     offspring = [parent_a[0],parent_b[1]]
+
+    mutate_chromosome(offspring)
 
     return offspring
     
 
 ########## Mutation  ###############
 
-def random_ind_gen(chromosome):
 
-    return random.randint(0, len(chromosome) - 1), random.randint(0, len(chromosome) - 1)
-
-def mutate_population(population):
-
-    mutated_population = []
-
-    for chromosome in population :
-
-        if 0 <= random.uniform(0, 1) <= 0.5:
-
-            mutated_chromosome = mutate_chromosome(chromosome)
-
-            mutate_population.append(mutated_chromosome)
-
-        else :
-
-            mutated_population.append(chromosome)
-
-
-def mutate_chromosome(chromosome,index):
-
-    index_1, index_2 = index
+def mutate_chromosome(chromosome):
 
     # Swap members at given indices
-    chromosome[index_1], chromosome[index_2] = chromosome[index_2], chromosome[index_1]
+    chromosome[0], chromosome[1] = chromosome[1], chromosome[0]
 
     return chromosome
 
 
 
+##### Truncation ########
+
+def truncate(population):
+
+    population.sort(key=eq_max.function_init,reverse=True)
+    del population[5:]
+
+    return population
 
 
-print(crossover(parents_tournament(population,random_fitness(population)),4))
+
+
+
+
+
+
+
 
